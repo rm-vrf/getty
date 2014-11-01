2a63e90a11c0d02f994e9c88e7e7eb345070c57b
lane.cn <lane.cn@gmail.com>
2014-11-01 11:51:36 +0800

    完成classpath资源服务，扩展名解析似乎有问题。不管了，先搞目录浏览吧。

--- src/main/java/cn/batchfile/getty/mvc/RequestMapping.java
+++ src/main/java/cn/batchfile/getty/mvc/RequestMapping.java
@@ -1,9 +1,16 @@
 package cn.batchfile.getty.mvc;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
@@ -11,7 +18,9 @@ import cn.batchfile.getty.configuration.Configuration;
 import cn.batchfile.getty.exceptions.ListDirectoryNotAllowedException;
 
 public class RequestMapping {
+	public static final String CLASSPATH_PREFIX = "/-cp";
 	private static final Logger logger = Logger.getLogger(RequestMapping.class);
+	private Map<String, File> classpathFiles = new ConcurrentHashMap<String, File>();
 	private Configuration configuration;
 	
 	public void setConfiguration(Configuration configuration) {
@@ -22,6 +31,10 @@ public class RequestMapping {
 		String uri = request.getRequestURI();
 		String context = configuration.getContextPath();
 		uri = StringUtils.substring(uri, context.length());
+		
+		if (StringUtils.startsWith(uri, CLASSPATH_PREFIX)) {
+			return findClasspathResource(StringUtils.substring(uri, CLASSPATH_PREFIX.length()));
+		}
 		
 		String path = configuration.getBaseDirectory() + configuration.getWebRoot() + uri;
 		if (logger.isDebugEnabled()) {
@@ -47,5 +60,43 @@ public class RequestMapping {
 		} else {
 			return file;
 		}
+	}
+	
+	private File findClasspathResource(String uri) {
+		if (StringUtils.startsWith(uri, "/")) {
+			uri = StringUtils.substring(uri, 1);
+		}
+		
+		if (!classpathFiles.containsKey(uri)) {
+			initClasspathFile(uri);
+		}
+		
+		return classpathFiles.get(uri);
+	}
+	
+	private String getExtension(String file) {
+		String name = StringUtils.substringAfterLast(file, "/");
+		return StringUtils.substringAfterLast(name, ".");
+	}
+	
+	synchronized private void initClasspathFile(String uri) {
+		String ext = getExtension(uri);
+		String tmpFile = System.getProperty("java.io.tmpdir", "/tmp");
+		tmpFile += "/" + UUID.randomUUID().toString() + "." + ext;
+		
+		File file = new File(tmpFile);
+		InputStream stream = null;
+		OutputStream output = null;
+		try {
+			stream = getClass().getClassLoader().getResourceAsStream(uri);
+			output = new FileOutputStream(file);
+			IOUtils.copy(stream, output);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		} finally {
+			IOUtils.closeQuietly(stream);
+			IOUtils.closeQuietly(output);
+		}
+		classpathFiles.put(uri, file);
 	}
 }
