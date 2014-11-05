cab8fa5fe7c21ebbbebf2069390fc583b8f8d19b
lane.cn <lane.cn@gmail.com>
2014-11-05 13:41:14 +0800

    修改错误的stat页面属性

--- webapp/samples/stat.groovy
+++ webapp/samples/stat.groovy
@@ -1,7 +1,7 @@
 def jvm = ['processor': Runtime.getRuntime().availableProcessors()]
 jvm.maxMemory = (Runtime.getRuntime().maxMemory() / 1024 / 1024) + "MB"
-jvm.totalMemory = (Runtime.getRuntime().maxMemory() / 1024 / 1024) + "MB"
-jvm.freeMemory = (Runtime.getRuntime().maxMemory() / 1024 / 1024) + "MB"
+jvm.totalMemory = (Runtime.getRuntime().totalMemory() / 1024 / 1024) + "MB"
+jvm.freeMemory = (Runtime.getRuntime().freeMemory() / 1024 / 1024) + "MB"
 
 def map = ['ok': true]
 map.env = System.getenv()
