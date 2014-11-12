e1c56069c0b9496b862d6a1dff9b448ded20cd74
lane.cn <lane.cn@gmail.com>
2014-11-12 16:58:34 +0800

    为目录浏览增加浏览readme页面

--- src/main/java/cn/batchfile/getty/parser/DirectoryParser.java
+++ src/main/java/cn/batchfile/getty/parser/DirectoryParser.java
@@ -1,6 +1,7 @@
 package cn.batchfile.getty.parser;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -11,6 +12,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
 import cn.batchfile.getty.configuration.Configuration;
@@ -32,16 +34,17 @@ public class DirectoryParser extends Parser {
 		response.setCharacterEncoding(configuration.charset());
 		
 		head(dir, request, response);
-		body(dir, request, response);
+		tableHead(dir, request, response);
+		File readme = tableBody(dir, request, response);
+		tableTail(dir, request, response);
+		if (readme != null) {
+			readme(readme, request, response);
+		}
 		tail(dir, request, response);
 	}
 	
 	private void head(File dir, HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String title = request.getRequestURI();
-		String parentDir = StringUtils.substringBeforeLast(title, "/");
-		if (StringUtils.isEmpty(parentDir)) {
-			parentDir = "/";
-		}
 		
 		response.getWriter().println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">");
 		response.getWriter().println("<html>");
@@ -50,17 +53,23 @@ public class DirectoryParser extends Parser {
 		response.getWriter().println(" </head>");
 		response.getWriter().println(" <body>");
 		response.getWriter().println("<h1>Index of " + title + "</h1>");
+	}
+	
+	private void tableHead(File dir, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String title = request.getRequestURI();
+		String parentDir = StringUtils.substringBeforeLast(title, "/");
+		if (StringUtils.isEmpty(parentDir)) {
+			parentDir = "/";
+		}
+		
 		response.getWriter().println("  <table>");
 		response.getWriter().println("   <tr><th valign=\"top\"><img src=\"/_cp/images/blank.png\" alt=\"[ICO]\"></th><th>Name</th><th>Last modified</th><th>Size</th><th>Description</th></tr>");
 		response.getWriter().println("   <tr><th colspan=\"5\"><hr></th></tr>");
 		response.getWriter().println("<tr><td valign=\"top\"><img src=\"/_cp/images/back.png\" alt=\"[PARENTDIR]\"></td><td><a href=\"" + parentDir + "\">Parent Directory</a></td><td>&nbsp;</td><td align=\"right\">  - </td><td>&nbsp;</td></tr>");
 	}
 	
-	private String getComparatorName(File file) {
-		return String.format("%s:%s", file.isDirectory() ? "D" : "F", file.getName().toLowerCase());
-	}
-	
-	private void body(File dir, HttpServletRequest request, HttpServletResponse response) throws IOException {
+	private File tableBody(File dir, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		File readme = null;
 		List<File> files = Arrays.asList(dir.listFiles());
 		Collections.sort(files, new Comparator<File>() {
 			@Override
@@ -73,6 +82,10 @@ public class DirectoryParser extends Parser {
 		
 		for (File file : files) {
 			String name = file.getName();
+			if (StringUtils.equalsIgnoreCase("readme.html", name) ||
+					StringUtils.equals("readme.html", name)) {
+				readme = file;
+			}
 			String href = request.getRequestURI();
 			if (!StringUtils.endsWith(href, "/")) {
 				href += "/";
@@ -82,19 +95,37 @@ public class DirectoryParser extends Parser {
 			String type = file.isDirectory() ? "DIR" : "FILE";
 			String icon = file.isDirectory() ? "folder" : "generic";
 			String size = getSize(file);
-			response.getWriter().println("<tr><td valign=\"top\"><img src=\"/_cp/images/" + icon + ".png\" alt=\"[" + type + "]\"></td><td><a href=\"" + href + "\">" + name + "</a></td><td align=\"right\">" + time + "</td><td align=\"right\">  " + size + " </td><td>&nbsp;</td></tr>");
+			response.getWriter().println("<tr><td valign=\"top\"><img src=\"/_cp/images/" + icon + ".png\" alt=\"[" + type + "]\"></td><td><a href=\"" + href + "\">" + name + "</a></td><td align=\"right\"> &nbsp; " + time + "</td><td align=\"right\"> &nbsp; " + size + " </td><td>&nbsp;</td></tr>");
 		}
+		
+		return readme;
 	}
 	
+	private void tableTail(File dir, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getWriter().println("   <tr><th colspan=\"5\"><hr></th></tr>");
+		response.getWriter().println("</table>");
+	}
+	
+	private void readme(File readme, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		List<String> lines = IOUtils.readLines(new FileInputStream(readme));
+		for (String line : lines) {
+			response.getWriter().println(line);
+		}
+	}
+
+	private void tail(File dir, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getWriter().println("</body></html>");
+	}
+
 	private String getSize(File file) {
 		if (file.isDirectory()) {
 			return "-";
 		}
 		float length = file.length();
 		if (length == 0) {
-			return "0B";
+			return "0";
 		} else if (length < 1024) {
-			return String.format("%.1fB", (float)length);
+			return String.format("%.0f", (float)length);
 		} else if (length < 1024 * 1024) {
 			return String.format("%.1fK", (float)(length / 1024F));
 		} else if (length < 1024 * 1024 * 1024) {
@@ -103,10 +134,9 @@ public class DirectoryParser extends Parser {
 			return String.format("%.1fG", (float)(length / 1024F / 1024F / 1024F));
 		}
 	}
-
-	private void tail(File dir, HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.getWriter().println("   <tr><th colspan=\"5\"><hr></th></tr>");
-		response.getWriter().println("</table>");
-		response.getWriter().println("</body></html>");
+	
+	private String getComparatorName(File file) {
+		return String.format("%s:%s", file.isDirectory() ? "D" : "F", file.getName().toLowerCase());
 	}
+	
 }
